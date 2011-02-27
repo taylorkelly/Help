c584695cb5af3f5434307eb1e442fb62f671c1e7
Taylor Kelly <tkelly910@gmail.com>
2011-02-27 23:58:43 +0100

    separation of main and plugin helps

--- src/main/java/me/taylorkelly/help/HelpList.java
+++ src/main/java/me/taylorkelly/help/HelpList.java
@@ -10,15 +10,18 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 public class HelpList {
-    private HashMap<String, HelpEntry> helpList;
+
+    private HashMap<String, HelpEntry> mainHelpList;
+    private HashMap<String, HashMap<String, HelpEntry>> pluginHelpList;
 
     HelpList() {
-        helpList = new HashMap<String, HelpEntry>();
+        mainHelpList = new HashMap<String, HelpEntry>();
+        pluginHelpList = new HashMap<String, HashMap<String, HelpEntry>>();
     }
 
     public ArrayList<HelpEntry> getSortedHelp(Player player, int start, int size) {
         ArrayList<HelpEntry> ret = new ArrayList<HelpEntry>();
-        List<String> names = new ArrayList<String>(helpList.keySet());
+        List<String> names = new ArrayList<String>(mainHelpList.keySet());
         Collator collator = Collator.getInstance();
         collator.setStrength(Collator.SECONDARY);
         Collections.sort(names, collator);
@@ -27,7 +30,7 @@ public class HelpList {
         int currentCount = 0;
         while (index < names.size() && ret.size() < size) {
             String currName = names.get(index);
-            HelpEntry entry = helpList.get(currName);
+            HelpEntry entry = mainHelpList.get(currName);
             if (entry.playerCanUse(player)) {
                 if (currentCount >= start) {
                     ret.add(entry);
@@ -41,12 +44,12 @@ public class HelpList {
     }
 
     public int getSize() {
-        return helpList.size();
+        return mainHelpList.size();
     }
 
     public double getMaxEntries(Player player) {
         int count = 0;
-        for (HelpEntry entry : helpList.values()) {
+        for (HelpEntry entry : mainHelpList.values()) {
             if (entry.playerCanUse(player)) {
                 count++;
             }
@@ -54,27 +57,94 @@ public class HelpList {
         return count;
     }
 
+    public ArrayList<HelpEntry> getSortedHelp(Player player, int start, int size, String plugin) {
+        ArrayList<HelpEntry> ret = new ArrayList<HelpEntry>();
+        if (pluginHelpList.containsKey(plugin)) {
+            return ret;
+        } else {
+            List<String> names = new ArrayList<String>(pluginHelpList.get(plugin).keySet());
+            Collator collator = Collator.getInstance();
+            collator.setStrength(Collator.SECONDARY);
+            Collections.sort(names, collator);
+
+            int index = 0;
+            int currentCount = 0;
+            while (index < names.size() && ret.size() < size) {
+                String currName = names.get(index);
+                HelpEntry entry = pluginHelpList.get(plugin).get(currName);
+                if (entry.playerCanUse(player)) {
+                    if (currentCount >= start) {
+                        ret.add(entry);
+                    } else {
+                        currentCount++;
+                    }
+                }
+                index++;
+            }
+            return ret;
+        }
+    }
+
+    public int getSize(String plugin) {
+        if (pluginHelpList.containsKey(plugin)) {
+            return pluginHelpList.get(plugin).size();
+        } else {
+            return 0;
+        }
+    }
+
+    public double getMaxEntries(Player player, String plugin) {
+        if (pluginHelpList.containsKey(plugin)) {
+            int count = 0;
+            for (HelpEntry entry : pluginHelpList.get(plugin).values()) {
+                if (entry.playerCanUse(player)) {
+                    count++;
+                }
+            }
+            return count;
+        } else {
+            return 0;
+        }
+    }
+
     public boolean registerCommand(String command, String description, Plugin plugin) {
         HelpEntry entry = new HelpEntry(command, description, plugin);
-        helpList.put(command, entry);
+        saveEntry(plugin, entry);
         return true;
     }
 
     public boolean registerCommand(String command, String description, Plugin plugin, boolean main) {
         HelpEntry entry = new HelpEntry(command, description, plugin, main);
-        helpList.put(command, entry);
+        if (main) {
+            mainHelpList.put(command, entry);
+        }
+        saveEntry(plugin, entry);
         return true;
     }
 
     public boolean registerCommand(String command, String description, Plugin plugin, String[] permissions) {
         HelpEntry entry = new HelpEntry(command, description, plugin, permissions);
-        helpList.put(command, entry);
+        saveEntry(plugin, entry);
         return true;
     }
 
     public boolean registerCommand(String command, String description, Plugin plugin, boolean main, String[] permissions) {
         HelpEntry entry = new HelpEntry(command, description, plugin, main, permissions);
-        helpList.put(command, entry);
+        if (main) {
+            mainHelpList.put(command, entry);
+        }
+        saveEntry(plugin, entry);
         return true;
+    }
+
+    private void saveEntry(Plugin plugin, HelpEntry entry) {
+        String name = plugin.getDescription().getName();
+        if (pluginHelpList.containsKey(name)) {
+            pluginHelpList.get(name).put(entry.command, entry);
+        } else {
+            HashMap<String, HelpEntry> map = new HashMap<String, HelpEntry>();
+            map.put(entry.command, entry);
+            pluginHelpList.put(name, map);
+        }
     }
 }
